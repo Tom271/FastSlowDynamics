da975e9680b84304ffdb09b8aa9ffaa6ea7dd7bf
Tom <Tom271@users.noreply.github.com>
2018-11-28 13:17:26 +0000

    VDP

--- Code/vdpPlot.m
+++ Code/vdpPlot.m
@@ -3,7 +3,7 @@ function vdpPlot= vdpPlot(epsilon,lambda,tSpan,initialValue,xyPlot,tPlot)
 %Plot solutions to the vdP equation with mu=m over the interval [0 
 % tSpan] from the point initialValue in the x-y plane.
     assert(epsilon>=0, 'm must be greater than zero')
-    tolerance=1/1000;
+    tolerance=1/10;
     if(epsilon<tolerance)
         [t,x]=ode15s(@(t,x) vdp(t,x,epsilon,lambda),[0 tSpan],initialValue);
     end
@@ -27,9 +27,7 @@ function vdpPlot= vdpPlot(epsilon,lambda,tSpan,initialValue,xyPlot,tPlot)
         dquivx=-quivy-quivx.^3/3+quivx.^2;
         dquivy=epsilon*(-lambda+dquivx);
         
-        
-        for j = 1:length(x(:,1))
-            hold on
+        hold on
             title(sprintf('Epsilon= %.3f, Initial point =(%d,%d)',epsilon,initialValue))
             axis( [-1,3, -1,3] )
             quiver(quivx,quivy,dquivx,dquivy,'Color','b')
@@ -38,24 +36,36 @@ function vdpPlot= vdpPlot(epsilon,lambda,tSpan,initialValue,xyPlot,tPlot)
             plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
             plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
             plot(ones(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150])
-            plot(x(1:j,1),x(1:j,2),'-o','Color','r','MarkerIndices',1:(j-1):j);
-            hold off
-            Mv(j) = getframe(fig);
-            children = get(fig, 'children');
+            plot(x(:,1),x(:,2),'-','Color','r');
             
-            if(j~=length(x(:,1)))
-                delete(children(1));
-            end 
-         
-        end
-        %movie(Mv,1);
-        v = VideoWriter('vdP.mp4','MPEG-4');
-        v.Quality=100;
-        v.FrameRate=30;
-
-        open(v)
-        writeVideo(v,Mv)
-        close(v)
+%         for j = 1:length(x(:,1))
+%             hold on
+%             title(sprintf('Epsilon= %.3f, Initial point =(%d,%d)',epsilon,initialValue))
+%             axis( [-1,3, -1,3] )
+%             quiver(quivx,quivy,dquivx,dquivy,'Color','b')
+%             plot(nullx,nully,'color','black','LineWidth',1.5)
+%             plot(fold1(1),fold1(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+%             plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+%             plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
+%             plot(ones(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150])
+%             plot(x(1:j,1),x(1:j,2),'-o','Color','r','MarkerIndices',1:(j-1):j);
+%             hold off
+%             Mv(j) = getframe(fig);
+%             children = get(fig, 'children');
+%             
+%             if(j~=length(x(:,1)))
+%                 delete(children(1));
+%             end 
+%          
+%         end
+%         movie(Mv,1);
+%         v = VideoWriter('vdP.mp4','MPEG-4');
+%         v.Quality=100;
+%         v.FrameRate=30;
+% 
+%         open(v)
+%         writeVideo(v,Mv)
+%         close(v)
         
     end
     if(tPlot==1)
@@ -66,3 +76,8 @@ function vdpPlot= vdpPlot(epsilon,lambda,tSpan,initialValue,xyPlot,tPlot)
     end
     
 end
+function vdp =vdp(t,x,epsilon,lambda)
+    vdp=[-x(2)-x(1).^3/3+x(1).^2;epsilon*(-lambda+x(1))];
+end 
+
+
