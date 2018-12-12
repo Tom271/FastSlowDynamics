4a1208064c6fad4e0ebc0544445340884509514f
Tom <Tom271@users.noreply.github.com>
2018-12-12 14:08:20 +0000

    Add title

--- Code/NumMethods/EvdpPlot.m
+++ Code/NumMethods/EvdpPlot.m
@@ -4,13 +4,14 @@ function [t,x]= EvdpPlot(epsilon,nu,a,b,c,tSpan,initialValue)
     % tSpan] from the point initialValue in the x-y plane.
     close all
     assert(epsilon>=0, 'm must be greater than zero')
-    
-    [t,x]=ode15s(@(t,x) EVdP(t,x,epsilon,nu,a,b,c),tSpan,initialValue);
-    
-    figure(1)
+    options = odeset('Stats','on','OutputFcn',@odeplot) ;
+    tic
+    [t,x]=ode15s(@(t,x) EVdP(t,x,epsilon,nu,a,b,c),tSpan,initialValue,options);
+    toc
+    figure(2)
     plot(t,x(:,1))
     
-    figure(2)
+    figure(3)
     %subplot(2,1,2)
     hold on
     plot3(x(:,1),x(:,2),x(:,3))
@@ -19,13 +20,13 @@ function [t,x]= EvdpPlot(epsilon,nu,a,b,c,tSpan,initialValue)
     zlabel('z')
     
     [X,Z]=meshgrid(-1:0.01:0.4,-0.08:0.001:0.04);
-	s=surf(X,X.^3+X.^2,Z);
+	s=surf(X,X.^2+X.^3,Z);
     s.EdgeColor='none';
     s.FaceColor='c';
     s.FaceAlpha=0.25;
     plot(X,X.^3-X.^2,'k','LineWidth',10)
     xlim([-1,0.4])
-    ylim([-0.01, 0.21])
+    ylim([-0.02, 0.22])
     zlim([-0.08,0.04])
     
 end
