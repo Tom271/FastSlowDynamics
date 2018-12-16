a168456c0a0acf11f10e0dab63a85a096a7c729f
HODGSON Thomas <s1415551@ed.ac.uk>
2018-12-16 17:30:30 +0000

    update Intro

--- Code/vdpPlot.m
+++ Code/vdpPlot.m
@@ -27,47 +27,49 @@ close all
         [quivx,quivy]=meshgrid(-1.5:0.1:3.1);
         dquivx=-quivy-quivx.^3/3+quivx.^2;
         dquivy=epsilon*(-lambda+dquivx);
+        
+        hold on
+        %title(sprintf('Epsilon= %.3f, Initial point =(%d,%d)',epsilon,initialValue))
+        axis( [-1.51,3.1, -1.5,3.1] )
+        quiver(quivx,quivy,dquivx,dquivy,'Color','b')
+        plot(nullx,nully,'color','black','LineWidth',1.5)
+        %plot(fold1(1),fold1(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+        %plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
+        plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
+        plot(zeros(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150])
+        plot(x(:,1),x(:,2),'Color','r');
+        
+        hold off
+
+%          for j = 1:length(x(:,1))
 %             hold on
 %             title(sprintf('Epsilon= %.3f, Initial point =(%d,%d)',epsilon,initialValue))
-%             axis( [-1.51,3.1, -1.5,3.1] )
+%             axis( [-1.5,3.1, -1.5,3.1] )
 %             quiver(quivx,quivy,dquivx,dquivy,'Color','b')
 %             plot(nullx,nully,'color','black','LineWidth',1.5)
 %             plot(fold1(1),fold1(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
 %             plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
 %             plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
-%             plot(zeros(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150])
-%             plot(x(:,1),x(:,2),'Color','r');
+%             plot(ones(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150]) %change to zero in canard
+%             plot(x(1:j,1),x(1:j,2),'-o','Color','r','MarkerIndices',1:(j-1):j);
 %             hold off
-        
-         for j = 1:length(x(:,1))
-            hold on
-            title(sprintf('Epsilon= %.3f, Initial point =(%d,%d)',epsilon,initialValue))
-            axis( [-1.5,3.1, -1.5,3.1] )
-            quiver(quivx,quivy,dquivx,dquivy,'Color','b')
-            plot(nullx,nully,'color','black','LineWidth',1.5)
-            plot(fold1(1),fold1(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
-            plot(fold2(1),fold2(2),'g-s','MarkerFaceColor','g','MarkerSize',10)
-            plot(equil(1),equil(2),'b-s','MarkerFaceColor','b','MarkerSize',10)
-            plot(ones(1,length(null2y)),null2y,'--','LineWidth',1.5,'Color',1/255*[150,150,150]) %change to zero in canard
-            plot(x(1:j,1),x(1:j,2),'-o','Color','r','MarkerIndices',1:(j-1):j);
-            hold off
-            Mv(j) = getframe(fig);
-            children = get(fig, 'children');
-            
-            if(j~=length(x(:,1)))
-                delete(children(1));
-            end 
-        
-      end
-        movie(Mv,1);
-        v = VideoWriter('vdP.mp4','MPEG-4');
-        v.Quality=100;
-        v.FrameRate=30;
-
-        open(v)
-        writeVideo(v,Mv)
-        close(v)
-        
+%             Mv(j) = getframe(fig);
+%             children = get(fig, 'children');
+%             
+%             if(j~=length(x(:,1)))
+%                 delete(children(1));
+%             end 
+%         
+%         end
+%         movie(Mv,1);
+%         v = VideoWriter('vdP.mp4','MPEG-4');
+%         v.Quality=100;
+%         v.FrameRate=30;
+% 
+%         open(v)
+%         writeVideo(v,Mv)
+%         close(v)
+%         
     end
     if(tPlot==1)
         figure();
